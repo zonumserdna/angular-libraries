c567013ee30ae6af62b553ff9554c44f283222ff
Andres Munoz <amun02@hotmail.com>
2019-01-27 16:39:55 -0500

    New angular library created

---
+++ projects/example-ng6-lib/src/lib/example-ng6-lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ExampleNg6LibComponent } from './example-ng6-lib.component';
+
+@NgModule({
+  declarations: [ExampleNg6LibComponent],
+  imports: [
+  ],
+  exports: [ExampleNg6LibComponent]
+})
+export class ExampleNg6LibModule { }
