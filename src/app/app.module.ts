aca1f880e73250fbb49de260a3bfac081761fa0a
Andres Munoz <amun02@hotmail.com>
2019-01-27 17:18:23 -0500

    Use the default created library-component in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ExampleNg6LibModule} from 'example-ng6-lib';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleNg6LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
