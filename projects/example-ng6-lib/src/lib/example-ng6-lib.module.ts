7a91ace35ee0101176be6502fe5dd0ad61269dcd
Andres Munoz <amun02@hotmail.com>
2019-01-27 19:32:26 -0500

    New foo component created in the angular library

--- projects/example-ng6-lib/src/lib/example-ng6-lib.module.ts
+++ projects/example-ng6-lib/src/lib/example-ng6-lib.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ExampleNg6LibComponent } from './example-ng6-lib.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
-  declarations: [ExampleNg6LibComponent],
+  declarations: [ExampleNg6LibComponent, FooComponent],
   imports: [
   ],
   exports: [ExampleNg6LibComponent]
